24ca06245b6dda083e7ae5e81724b66ae5b1c370
Michal Gwiazdonik <michal.gwiazdonik@gmail.com>
2021-02-10 17:16:58 +0100

    Add collateral controller

---
+++ app/controllers/collaterals_controller.rb
@@ -0,0 +1,5 @@
+class CollateralsController < ApplicationController
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root "collaterals#index"
+
+  get "/collaterals", to: "collaterals#index"
 end
