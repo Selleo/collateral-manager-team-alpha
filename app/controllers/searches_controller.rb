eb0c8cf65027b5d6c3cfff55893d1c589ff0e591
Michal Gwiazdonik <michal.gwiazdonik@gmail.com>
2021-02-15 18:46:44 +0100

    Add search querries

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,9 +2,16 @@ class SearchesController < ApplicationController
 
   def new
 
+    @tag = Tag.joins(:collaterals_tags).distinct
+
   end
 
   def create
+
+    @tag = Tag.new(params)
+
+    @collateral = Collateral.joins(collaterals_tags: :tag).where(tags: {id: test}).distinct
+
     # @collateral = Collateral.new(collateral_params)
     #
     # if @collateral.save
@@ -12,5 +19,10 @@ class SearchesController < ApplicationController
     # else
     #   render :new
     # end
+  end
+
+  private
+  def params
+    params.require(:tag).permit(:id)
   end
 end
