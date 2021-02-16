7f477e7c4af0b39907c133cf7986fb510573314f
Michnikowski <michal.gwiazdonik@gmail.com>
2021-02-16 08:55:58 +0100

    Change view search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,12 +7,11 @@ class SearchesController < ApplicationController
   def create
 
     tags_array = params[:search][:tag_ids]
-    @collaterals = Collateral.joins(collaterals_tags: :tag).where(tags: {id: tags_array}).distinct
-    render :show #or 'collateralsindex'
-  end
 
-  private
-  def tag_params
-    params.require(:search).permit
+    @collaterals = Collateral.joins(collaterals_tags: :tag).where(tags: {id: tags_array}).distinct
+
+    @tag = Tag.where(id: tags_array).order(:category, :name)
+
+    render :show #or 'collateralsindex'
   end
 end
