a7dacf3fcd586c1e76a40e3397cbabc6bf69a88e
Wioletta Piesiak <wiola.piesiak@gmail.com>
2021-02-18 19:04:31 +0100

    base test

---
+++ spec/collections/top_matching_collateral_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe TopMatchingCollateral do
+  context 'list' do
+    it 'returns all matching collaterals' do
+
+    end
+
+  end
+end
--- spec/controllers/collaterals_tags_controller_spec.rb
+++ spec/controllers/collaterals_tags_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe CollateralsTagsController, type: :controller do
+  context 'connect collateral and tag' do
+    it 'return connected collateral' do
+      collateral = Collateral.create(title: "Title")
+      tag = Tag.create( name: 'tag')
+      collateral_tag = CollateralsTag.create(collateral: collateral, tag: tag, weight: 100)
 
+      expect(collateral_tag.collateral).to eq collateral
+    end
+    it 'return connected tag' do
+      collateral = Collateral.create(title: "Title")
+      tag = Tag.create( name: 'tag')
+      collateral_tag = CollateralsTag.create(collateral: collateral, tag: tag, weight: 100)
+
+      expect(collateral_tag.tag).to eq tag
+    end
+  end
 end
