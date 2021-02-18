65ff03685ce83d5e09e1d395bb05482edc04be82
Wioletta Piesiak <wiola.piesiak@gmail.com>
2021-02-18 18:28:57 +0100

    search-index

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,14 +1,16 @@
 class SearchesController < ApplicationController
 
-  def new
-    @tags = Tag.joins(:collaterals_tags).order(:name).distinct
-    @tags_gruped_by_category = @tags.group_by { |tag| tag[:category]}
-  end
-
   def index
-    tags_array = params[:search][:tag_ids]
-    @weighted_collaterals = Kaminari.paginate_array(TopMatchingCollateral.new(tags_array).sort_hash).page(params[:page])
-    @tags = Tag.where(id: tags_array).order(:category, :name)
+    tags = Tag.joins(:collaterals_tags).order(:name).distinct
+    @tags_grouped_by_category = tags.group_by { |tag| tag[:category]}
+
+    tags_array = params[:search].present? ? params[:search][:tag_ids] : []
+
+    if tags_array
+      @weighted_collaterals = Kaminari.paginate_array(TopMatchingCollateral.new(tags_array).sort_hash).page(params[:page])
+      @selected_tags = Tag.where(id: tags_array).order(:category, :name)
+    end
+
   end
 
 end
